afb29519c14c6b79ec0fe60d653b4a7348d7f159
Ben Mathes <mathes.ben@gmail.com>
2013-05-14 14:45:40 -0700

    initial rails commit
    remove old .gitignore
    
    adding gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarthquakeChallenge::Application.config.secret_token = 'b4f5f91665970cd8e3a81a1222982a1cbf7ff83d22ffcd9e5a94bcb3b139c19fea00899fc0ceaf7b48b328e1cf5b707d64bad70d5684c25ee157569a93ce85e1'
