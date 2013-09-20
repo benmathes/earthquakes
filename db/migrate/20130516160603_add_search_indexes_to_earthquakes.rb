91b174a552ede1533ec424857364ca001098716a
Ben Mathes <mathes.ben@gmail.com>
2013-09-20 00:51:02 -0700

    move the adding of search indecies into the table creation migration

--- db/migrate/20130516160603_add_search_indexes_to_earthquakes.rb
+++
@@ -1,9 +0,0 @@
-class AddSearchIndexesToEarthquakes < ActiveRecord::Migration
-  def change
-    add_index :earthquakes, :id, :unique => true
-    add_index :earthquakes, :time
-    add_index :earthquakes, :magnitude
-    add_index :earthquakes, :latitude
-    add_index :earthquakes, :longitude
-  end
-end
